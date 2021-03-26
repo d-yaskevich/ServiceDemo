90464edb5d965052894b784e3a48deff17348a4e
Dasha <Dasha.Yaskevich@cluster-one.eu>
2021-03-26 20:07:09 +0300

    Initial commit

---
+++ app/src/main/java/com/myfirst/servicedemo/MainActivity.java
@@ -0,0 +1,138 @@
+package com.myfirst.servicedemo;
+
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String BROADCAST_ACTION = "com.daria.example.servicebackbroadcast";
+    public static final String MAX_NUMBER_KEY = "MAX_NUMBER_KEY";
+    public static final String PENDING_INTENT_KEY = "PENDING_INTENT_KEY";
+
+    public static final int REQUEST_CODE = 4003;
+
+    private NumberPicker picker;
+    private TextView tvLog;
+
+    private Intent serviceIntent;
+    private ServiceConnection serviceConnection;
+    private MyService myService;
+    private boolean bound = false;
+
+    private BroadcastReceiver broadcastReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        picker = findViewById(R.id.np_number);
+        picker.setMinValue(1);
+        picker.setMaxValue(100);
+        picker.setValue(50);
+
+        tvLog = findViewById(R.id.tv_log);
+        tvLog.setMovementMethod(new ScrollingMovementMethod());
+
+        Intent intent = getIntent();
+        if (intent != null && intent.hasExtra(MyService.NUMBER_KEY)) {
+            int number = intent.getIntExtra(MyService.NUMBER_KEY, 0);
+            tvLog.append("Random number: " + number + "\n");
+        }
+
+        serviceIntent = new Intent(this, MyService.class);
+        serviceConnection = new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+                MainActivity.this.onServiceConnected(service);
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+                MainActivity.this.onServiceDisconnected();
+            }
+        };
+
+        broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                int number = intent.getIntExtra(MyService.NUMBER_KEY, 0);
+                tvLog.append("Random number: " + number + "\n");
+            }
+        };
+        IntentFilter intentFilter = new IntentFilter(BROADCAST_ACTION);
+        registerReceiver(broadcastReceiver, intentFilter);
+    }
+
+    private void onServiceConnected(IBinder service) {
+        tvLog.append("BOUND to service\n");
+        myService = ((MyService.MyBinder) service).getService();
+        myService.maxNumber = picker.getValue();
+        picker.setOnValueChangedListener((picker, oldVal, newVal) -> myService.maxNumber = newVal);
+        bound = true;
+    }
+
+    private void onServiceDisconnected() {
+        tvLog.append("UNBOUND from service\n");
+        myService = null;
+        picker.setOnValueChangedListener(null);
+        bound = false;
+    }
+
+    public void onStartService(View view) {
+        PendingIntent pendingIntent = createPendingResult(REQUEST_CODE, new Intent(), 0);
+
+        serviceIntent.putExtra(MAX_NUMBER_KEY, picker.getValue());
+        serviceIntent.putExtra(PENDING_INTENT_KEY, pendingIntent);
+        startService(serviceIntent);
+    }
+
+    public void onStopService(View view) {
+        stopService(serviceIntent);
+    }
+
+    public void onBindService(View view) {
+        bindService(serviceIntent, serviceConnection, BIND_AUTO_CREATE);
+    }
+
+    public void onUnbindService(View view) {
+        if (!bound) return;
+        unbindService(serviceConnection);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE) {
+            switch (resultCode) {
+                case MyService.START_KEY:
+                    tvLog.append("START service task\n");
+                    break;
+                case MyService.STOP_KEY:
+                    tvLog.append("STOP service task\n");
+                    break;
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(broadcastReceiver);
+        onUnbindService(null);
+        super.onDestroy();
+    }
+}
---
+++ app/src/main/java/com/myfirst/servicedemo/MyService.java
@@ -0,0 +1,155 @@
+package com.myfirst.servicedemo;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.Build;
+import android.os.Handler;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+
+import java.util.Random;
+
+public class MyService extends Service {
+
+    private static final String TAG = MyService.class.getSimpleName();
+
+    private static final String CHANNEL_ID = "myservice.channel.id";
+    private static final long TIME_DELAY_MILLIS = 5000;
+
+    public static final String NUMBER_KEY = "NUMBER_KEY";
+    public static final int START_KEY = 1002;
+    public static final int STOP_KEY = 1003;
+
+    private Handler handler;
+    private Runnable runnable;
+
+    private PendingIntent pendingIntent;
+    private NotificationManager notificationManager;
+
+    public int maxNumber;
+
+    public MyService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i(TAG, "onCreate()");
+        handler = new Handler();
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        createNotificationChannel();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        maxNumber = intent.getIntExtra(MainActivity.MAX_NUMBER_KEY, 100);
+        Log.i(TAG, "onStartCommand() - max number = " + maxNumber + ";");
+
+        pendingIntent = intent.getParcelableExtra(MainActivity.PENDING_INTENT_KEY);
+        try {
+            if (pendingIntent != null) {
+                pendingIntent.send(START_KEY);
+            }
+        } catch (PendingIntent.CanceledException e) {
+            e.printStackTrace();
+        }
+
+        runnable = this::showRandomNumber; // == new Runnable() { ... }
+        handler.postDelayed(runnable, TIME_DELAY_MILLIS);
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    public void showRandomNumber() {
+        Random rand = new Random();
+        int number = rand.nextInt(maxNumber);
+        Log.i(TAG, "showRandomNumber() - number = " + number + ";");
+
+        Intent intent = new Intent(MainActivity.BROADCAST_ACTION);
+        intent.putExtra(NUMBER_KEY, number);
+        sendBroadcast(intent);
+
+        sendNotification(number);
+
+        handler.postDelayed(runnable, TIME_DELAY_MILLIS);
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    private void sendNotification(int number) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra(NUMBER_KEY, number);
+        PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Notification notification =
+                new NotificationCompat.Builder(this, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.ic_ac_unit)
+                        .setContentTitle("My Service title")
+                        .setContentText("Random number - " + number)
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                        .setContentIntent(pIntent)
+                        .setAutoCancel(true)
+                        .build();
+
+        notificationManager.notify(1, notification);
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i(TAG, "onDestroy()");
+        handler.removeCallbacks(runnable);
+        try {
+            if (pendingIntent != null) {
+                pendingIntent.send(STOP_KEY);
+            }
+        } catch (PendingIntent.CanceledException e) {
+            e.printStackTrace();
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.i(TAG, "onBind()");
+        return new MyBinder();
+    }
+
+    @Override
+    public void onRebind(Intent intent) {
+        super.onRebind(intent);
+        Log.i(TAG, "onRebind()");
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.i(TAG, "onUnbind()");
+        return true;
+    }
+
+    class MyBinder extends Binder {
+        public MyService getService() {
+            return MyService.this;
+        }
+    }
+}
